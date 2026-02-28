50cb40e87e66500a784fa648f2e8452e08e7e638
Babithosh <ellu250071@cet.ac.in>
2026-02-28 18:11:31 +0530

    corrected link list traversal code

--- src/Solution.java
+++ src/Solution.java
@@ -29,19 +29,26 @@ public class Solution {
         
         ListIterator<Integer> iterator = list.listIterator();
         
-        System.out.print("Forward: ");
+                        System.out.print("Forward: ");
+        if (iterator.hasNext()) {
+            System.out.print(iterator.next());
+        }
         while (iterator.hasNext()) {
-            System.out.print(iterator.next() + " ");
+            System.out.print(" " + iterator.next());
         }
         System.out.println();
         
         System.out.print("Backward: ");
         while (iterator.hasPrevious()) {
-            System.out.print(iterator.previous() + " ");
+            System.out.print(iterator.previous());
+            if (iterator.hasPrevious()) {
+                System.out.print(" ");
+            }
         }
+        System.out.println();
         
         scanner.close();
-        
+
         
     }
 }
